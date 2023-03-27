f0be16a777d956b63493314bd63df67e5fcb94cf
SoloM27 <126669726+SoloM27@users.noreply.github.com>
2023-03-27 14:28:45 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,40 +4,41 @@ let Irr = 0
 function HardLeft () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
-    basic.pause(100)
+    basic.pause(200)
 }
 function HardRight () {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
-    basic.pause(100)
+    basic.pause(200)
 }
 function avoid () {
+    avoiding = 1
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
-    basic.pause(100)
+    basic.pause(200)
     HardRight()
     while (avoiding == 1) {
-        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
-            SoftRight()
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 15) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
         } else {
             SoftLeft()
         }
-        if (IRL == 0 || Irr == 0) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 || maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
             avoiding = 0
             HardRight()
         }
     }
 }
 function SoftRight () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 80)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 40)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 45)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
 }
 function SoftLeft () {
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 80)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 45)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
 }
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
-        avoiding = 1
         avoid()
     }
     IRL = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
